302db95f5502f273ad3ba356b318656f4fc389c1
bruno <brunoantunes94@hotmail.com>
2020-01-18 17:43:24 -0300

    alteração de produtos

--- application/views/produtos.php
+++ application/views/produtos.php
@@ -10,11 +10,19 @@
 			<!-- Inicio da pagina -->
 					<div class="card-columns text-center mt-5">
 					<!-- inicia os cards-->
-						<div class="card">
+						<div class="card mb-5">
 							<img src="<?=base_url();?>content/imagens/pilsen.png" class="card-img-top img-fluid">
-							<div class="card-body">
-								<h4 class="card-title">Teclados</h4>
-								<div class="container-flud d-flex  justify-content-center">
+							<button class="btn btn-success btn-block card-footer" type="button" onclick="location.href='teclado.php'">Ver Mais</button>
+						</div>
+						<!-- finaliza os cards-->
+					
+					</div>		
+			<!--Fim da pagina-->
+		</div>
+	</div>
+</div>
+
+<div class="container-flud d-flex  justify-content-center">
 									<div class="row no-gutters">
 										<div class="col-6">
 											<p class="icon-sprite abv"></p>
@@ -22,7 +30,7 @@
 									<div class="col-6 display-5">
 										52
 									</div>
-									<div class="col-6">
+									<div class="col-6 mt-1">
 											<p class="icon-sprite grau"></p>
 										</div>
 									<div class="col-6 display-5">
@@ -42,13 +50,3 @@
 									</div>
 									</div>
 								</div>
-							</div>
-							<button class="btn btn-success btn-block card-footer" type="button" onclick="location.href='teclado.php'">Entre Aqui</button>
-						</div>
-						<!-- finaliza os cards-->
-					
-					</div>		
-			<!--Fim da pagina-->
-		</div>
-	</div>
-</div>
