bfbd35cb6b1153c94fdf4f596aaa33bb4be94b30
bruno <brunoantunes94@hotmail.com>
2020-01-17 21:43:31 -0300

    criação de produtos

--- application/views/produtos.php
+++ application/views/produtos.php
@@ -5,20 +5,50 @@
 </div>
 <div class="container clearfix mb-5">
 		<h1 class="display-4 text-center text-warning">Nossos Produtos</h1>
-	<div class="row">
-		<div class="col-md-12">
-			<div class="row w-100 mx-auto">
-				<div class="row">
-					<div class="col-md-10">
-						<div class="row">
-							<span class="icon-sprite abv"></span>
-							<span class="icon-sprite grau"></span>
-							<span class="icon-sprite ibu"></span>
-							<span class="icon-sprite ebc"></span>
-						</div>	
-					</div>
-				</div>
-			</div>
+			<div class="row">
+				<div class="col-md-12">
+			<!-- Inicio da pagina -->
+					<div class="card-columns text-center mt-5">
+					<!-- inicia os cards-->
+						<div class="card">
+							<img src="<?=base_url();?>content/imagens/pilsen.png" class="card-img-top img-fluid">
+							<div class="card-body">
+								<h4 class="card-title">Teclados</h4>
+								<div class="container-flud d-flex  justify-content-center">
+									<div class="row no-gutters">
+										<div class="col-6">
+											<p class="icon-sprite abv"></p>
+										</div>
+									<div class="col-6 display-5">
+										52
+									</div>
+									<div class="col-6">
+											<p class="icon-sprite grau"></p>
+										</div>
+									<div class="col-6 display-5">
+										25,2
+									</div>
+									<div class="col-6">
+											<p class="icon-sprite ibu"></p>
+										</div>
+									<div class="col-6 display-5">
+										25,2
+									</div>
+									<div class="col-6">
+											<p class="icon-sprite ebc"></p>
+										</div>
+									<div class="col-6 display-5">
+										25,2
+									</div>
+									</div>
+								</div>
+							</div>
+							<button class="btn btn-success btn-block card-footer" type="button" onclick="location.href='teclado.php'">Entre Aqui</button>
+						</div>
+						<!-- finaliza os cards-->
+					
+					</div>		
+			<!--Fim da pagina-->
 		</div>
 	</div>
 </div>
