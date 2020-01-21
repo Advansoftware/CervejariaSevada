9df77adf10f97036a950301ea5f1767b04fd221f
bruno <brunoantunes94@hotmail.com>
2020-01-20 23:20:59 -0300

    idade

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,5 +1,5 @@
 .<!doctype html>
-<html lang="en">
+<html lang="en" style="background-color: black;">
   <head>
     <title>Cervejaria Sevda - Bem Vindo</title>
     <!-- Required meta tags -->
@@ -8,12 +8,41 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/sevda.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/style.css">
   </head>
-  <body>
+  <body style="background-color: black; background-image: ;">
 
-
-  <div class="container-fluid">
-
+ 
+  <div class="container-fluid text-light text-center">
+    <div class="row justify-content-center">
+      <div class="col-md-3 col-sm-12 mt-5">
+        <img src="<?=base_url();?>content/imagens/LOGOMARCA.png" alt="logo" class="img-fluid mt-5">
+      </div>
+    </div>
+    <div class="row mt-5 justify-content-center">
+      <div class="col-md-3 col-sm-12 text-center">
+      <label for="idade">Digite Sua Data de Nascimento</label>
+        <form class="form-inline">
+          <div class="form-group mb-2 mt-2">
+            <div class="row">
+              <div class="col-md-6 col-sm-12 my-2">
+               <input type="date" class="form-control" id="idade" placeholder="Data de Nascimento">
+              </div>
+              <div class="col-md-6 col-sm-12 my-2">
+                <button type="submit" class="btn btn-success">Entrar No Site</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+      <div class="row mt-2 justify-content-center text-center">
+        <div class="col-12">
+        VOCÊ PRECISA TER MAIS DE 18 ANOS
+      PARA ACESSAR ESTE SITE.
+        </div>
+    </div>
 
   </div>
       
