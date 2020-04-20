69a9a6bc5c22c59c9aa33ff7d6173031b852ae03
bruno <brunoantunes94@hotmail.com>
2020-04-19 22:58:19 -0300

    remoção do slide

--- application/views/template/slide.php
+++ application/views/template/slide.php
@@ -2,20 +2,20 @@
 background-color: black;" data-ride="carousel">
 	<ol class="carousel-indicators">
 		<li data-target="#slides" data-slide-to="0" class="active"></li>
-		<li data-target="#slides" data-slide-to="1"></li>
-		<!---	<li data-target="#slides" data-slide-to="2"></li>
+		<!---<li data-target="#slides" data-slide-to="1"></li>
+			<li data-target="#slides" data-slide-to="2"></li>
 		-->
 	</ol>
 	<div class="carousel-inner">
 
 		<div class="carousel-item carousel-inicio-text active">
-				<img class="d-block mt-0 mb-5"  style="margin: 0px auto; width: 100% !important; max-height: 40rem;" src="<?php echo base_url();?>content/imagens/fotos/entrada.jpeg" alt="First slide">
+				<img class="d-block mt-0 mb-0"  style="margin: 0px auto; width: 100% !important; max-height: 40rem;" src="<?php echo base_url();?>content/imagens/fotos/entrada.jpeg" alt="First slide">
 				<div class="carousel-caption d-none carousel-caption-text d-md-block"> 
 					<h1 style="text-shadow: 3px 3px 7px rgba(0,0,0,1);text-transform:uppercase;" class="display-5">Cervejaria Sevda</h1>
 					<h4 style="text-shadow: 2px 3px 3px rgba(0,0,0,1);" class="my-3">Seja Bem Vindo!</h4>
 					<a href="#info" class="btn btn-rounded btn-success scrollSuave mt-4">Veja Mais</a>
 				</div>
-			</div>
+			</div><!--
 		<div class="carousel-item carousel-inicio-text">
 			<img class="d-block mt-0 mb-5"  style="margin: 0px auto; width: 100% !important; max-height: 40rem;" src="<?php echo base_url();?>content/imagens/slide.png" alt="First slide">
 			<div class="carousel-caption d-none carousel-caption-text d-md-block"> 
@@ -25,6 +25,7 @@ background-color: black;" data-ride="carousel">
 			</div>
 		</div>
 	</div>
+
 	<a class="carousel-control-prev" href="#slides" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -33,4 +34,6 @@ background-color: black;" data-ride="carousel">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
+-->
+</div>  
 </div>
